4fb9fcbe6266f3091d318a7cf0a1815f0eddcb7b
Ellen Ouyang <ouyel-26@rhodes.edu>
2026-03-23 01:34:28 -0500

    emergency fixes to hide new API keys for security purposes

---
+++ CadetLinks/src/firebase/app.config.ts
@@ -0,0 +1,19 @@
+// app.config.ts
+import "dotenv/config";
+
+export default {
+  expo: {
+    name: "CadetLinks",
+    slug: "cadetlinks",
+    extra: {
+      firebaseApiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+      firebaseAuthDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+      firebaseDatabaseURL: process.env.EXPO_PUBLIC_FIREBASE_DATABASE_URL,
+      firebaseProjectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+      firebaseStorageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+      firebaseMessagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+      firebaseAppId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+      firebaseMeasurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
+    },
+  },
+};
--- CadetLinks/src/firebase/config.ts
+++ CadetLinks/src/firebase/config.ts
@@ -1,14 +1,9 @@
+// firebase/config.ts
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBswld1nz8X-xXRYs_Q8wr5ihI0A0xvWWI",
   authDomain: "cadetlinks.firebaseapp.com",
@@ -17,10 +12,9 @@ const firebaseConfig = {
   storageBucket: "cadetlinks.firebasestorage.app",
   messagingSenderId: "111800465306",
   appId: "1:111800465306:web:0198344279e2876b952c23",
-  measurementId: "G-8FC6FRBYKP"
+  measurementId: "G-8FC6FRBYKP",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 export const db = getDatabase(app);
