7c7ebe6ecc85d53213db36e732e029110b362fbd
TGOB196 <62917251+TGOB196@users.noreply.github.com>
2026-04-07 16:17:42 -0500

    quick resolve to error message on ios app

--- CadetLinks/src/firebase/config.ts
+++ CadetLinks/src/firebase/config.ts
@@ -1,5 +1,6 @@
 // firebase/config.ts
 import { initializeApp } from "firebase/app";
+import { isSupported, getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 import { getStorage } from "firebase/storage";
@@ -17,6 +18,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+isSupported().then((supported) => { if (supported) getAnalytics(app); });
 export const db = getDatabase(app);
 export const auth = getAuth(app);
 export const storage = getStorage(app, "gs://cadetlinks.firebasestorage.app"); 
